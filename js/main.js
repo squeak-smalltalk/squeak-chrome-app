c62be66e293ac17433f4f36fe79368dbf9cd4689
Fabio Niephaus <code@fniephaus.com>
2016-02-17 22:27:50 +0000

    Forward keyboard events to sandbox; closes #1

--- js/main.js
+++ js/main.js
@@ -1,15 +1,46 @@
 window.onload = function() {
-    var squeakImages = document.getElementsByClassName('squeak-image');
-    for (var i = 0; i < squeakImages.length; i++) {
-        squeakImages[i].addEventListener('click', function(e) {
-            var files = this.getAttribute('data-files');
-            sqWelcome.style.display = "none";
-            document.getElementById('sqFrame').style.display = "block";
-            document.getElementById('sqFrame').contentWindow.postMessage(files, '*');
-        });
+    window.squeakImages = document.getElementsByClassName('squeak-image');
+    window.sandboxedFrame = document.getElementById('sqFrame');
+    window.sandboxedWindow = sandboxedFrame.contentWindow;
+    var i;
+    for (i = 0; i < squeakImages.length; i++) {
+        squeakImages[i].addEventListener('click', selectionHandler);
     }
     window.addEventListener('message', function(event) {
         sqWelcome.style.display = "block";
-    	document.getElementById('sqFrame').style.display = "none";
+    	sandboxedFrame.style.display = "none";
     });
+    var keyboardEventTypes = ['keydown', 'keyup', 'keypress'];
+    for (i = 0; i < keyboardEventTypes.length; i++) {
+        document.addEventListener(keyboardEventTypes[i], keyboardHandler);
+    }
 };
+
+function selectionHandler(e) {
+    var files = this.getAttribute('data-files');
+    sqWelcome.style.display = "none";
+    window.sandboxedFrame.style.display = "block";
+    window.sandboxedWindow.postMessage({files: files}, '*');
+}
+
+function keyboardHandler(e) {
+    window.sandboxedWindow.postMessage({event: copyKeyboardEvent(e)}, '*');
+}
+
+function copyKeyboardEvent(event) {
+    return {
+        altKey: event.altKey,
+        bubbles: event.bubbles,
+        cancelable: event.cancelable,
+        charCode: event.charCode,
+        code: event.code,
+        ctrlKey: event.ctrlKey,
+        keyCode: event.keyCode,
+        keyIdentifier: event.keyIdentifier,
+        location: event.location,
+        metaKey: event.metaKey,
+        shiftKey: event.shiftKey,
+        type: event.type,
+        which: event.which,
+    };
+}
--- js/sandbox.js
+++ js/sandbox.js
@@ -6,24 +6,56 @@ var IMAGE_BASE_URL = 'http://freudenbergs.de/bert/squeakjs/';
 
 window.onload = function() {
     window.addEventListener('message', function(event) {
-        var files = event.data.split(',');
-        var imageName = files[0];
-        SqueakJS.runSqueak(IMAGE_BASE_URL + imageName, sqCanvas, {
-            appName: imageName && imageName.replace(/\.image$/, ""),
-            files: files,
-            fullscreen: true,
-            swapButtons: true,
-            spinner: sqSpinner,
-            onQuit: function(vm, display, options) {
-                display.vm = null;
-                display.showBanner("Exiting...");
-                setTimeout(function() {
-                    event.source.postMessage('exit', event.origin);
-                    if (display.cursorCanvas.parentNode != null) {
-                      display.cursorCanvas.parentNode.removeChild(display.cursorCanvas);
-                    }
-                }, 500);
-            }
-        });
+        if (event.data.event !== undefined) {
+            dispatchClonedKeyboardEvent(event.data.event);
+        } else if (event.data.files !== undefined) {
+            var files = event.data.files.split(',');
+            var imageName = files[0];
+            SqueakJS.runSqueak(IMAGE_BASE_URL + imageName, sqCanvas, {
+                appName: imageName && imageName.replace(/\.image$/, ""),
+                files: files,
+                fullscreen: true,
+                swapButtons: true,
+                spinner: sqSpinner,
+                onQuit: function(vm, display, options) {
+                    display.vm = null;
+                    display.showBanner("Exiting...");
+                    setTimeout(function() {
+                        event.source.postMessage('exit', event.origin);
+                        var parentNode = display.cursorCanvas.parentNode;
+                        if (parentNode !== null) {
+                          parentNode.removeChild(display.cursorCanvas);
+                        }
+                    }, 500);
+                }
+            });
+        }
     });
 };
+
+function dispatchClonedKeyboardEvent(event) {
+    var keyboardEvent = document.createEvent("KeyboardEvent");
+    keyboardEvent.initKeyboardEvent(
+        event.type,
+        event.bubbles,
+        event.cancelable,
+        window,
+        event.keyIdentifier,
+        event.location,
+        event.ctrlKey,
+        event.altKey,
+        event.shiftKey,
+        event.metaKey,
+        false //event.altGraphKey
+    );
+    var override_props = ["charCode", "code", "keyCode", "which"];
+    for (var i = 0; i < override_props.length; i++) {
+        var prop_name = override_props[i];
+        delete keyboardEvent[prop_name];
+        Object.defineProperty(keyboardEvent, prop_name, {
+            writable: true,
+            value: event[prop_name],
+        });
+    }
+    document.dispatchEvent(keyboardEvent);
+}
